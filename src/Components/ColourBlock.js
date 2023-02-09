afaa6e89127dd62b5e13edced1738d9561e816d2
Joe Pointon <joepointon2@gmail.com>
2023-02-09 14:13:14 +0000

    formatting

--- src/Components/ColourBlock.js
+++ src/Components/ColourBlock.js
@@ -1,5 +1,9 @@
 function ColourBlock({ colour, id }) {
-  return <div style={{ backgroundColor: colour }}><h3>{id+1}</h3></div>;
+  return (
+    <div style={{ backgroundColor: colour }}>
+      <h3>{id + 1}</h3>
+    </div>
+  );
 }
 
 export default ColourBlock;
